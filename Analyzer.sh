6b6f42327f383a24fcba1666cfacb2d4a24e8af2
Urvps2025 <ugphone464@gmail.com>
2025-04-09 14:05:23 -0600

    Update Analyzer.sh

--- Analyzer.sh
+++ Analyzer.sh
@@ -2,6 +2,8 @@ sudo apt update && sudo apt install geoip-bin
 
 git clone https://github.com/Urvps2025/vps-ip-geolocation/
 
+cd vps-ip-geolocation
+
 chmod +x vps-ip-geolocation.sh
 
 ./vps-ip-geolocation.sh
